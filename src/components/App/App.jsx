5af84255fde682eca3a390fae0f5078bde981488
MickKrishtopa <m.a.krishtopa@yandex.ru>
2023-10-12 13:04:58 +0300

    feat: make component ContactUsLarge

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,11 @@
 import "./App.scss";
 import Layout from "../../Layout/Layout";
-import { ContactUsSmall } from "../ContactUsSmall/ContactUsSmall";
-import { ContactUsLarge } from "../ContactUsLarge/ContactUsLarge";
 
 const App = () => {
   return (
     <>
       <Layout>
         <h1>Develop</h1>
-        <ContactUsLarge />
-        {/* <ContactUsSmall /> */}
       </Layout>
     </>
   );
