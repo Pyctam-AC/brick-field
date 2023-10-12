bd0081720d537ca9f312c34c737197cb268399f1
Valeriy Nikonovich <nikonovi4v@yandex.ru>
2023-10-12 13:16:04 +0300

    header-done, footer - not yet

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,13 @@
 import "./Layout.scss";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export default function Layout({ children }) {
   return (
     <>
+    <Header />
       <main>{children}</main>
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <section className="footer__content">
+        <div className="footer__item">
+          <h1 className="footer__title">
+            Балтымский
+            <br /> Кирпичный&nbsp;ЗАВОД
+          </h1>
+          <h3 className="footer__address">
+            <b>Екатеринбург,</b> <br /> пос.&nbsp;Садовый,
+            <br /> ул.&nbsp;Валимхаматова, д.&nbsp;36{" "}
+          </h3>
+        </div>
+        <div className="footer__item">
+          <p className="footer__schedule">
+            Для заказов:
+            <br /> пн-пт
+            <br /> с&nbsp;8:00 до&nbsp;17:00
+          </p>
+          <ul className="footer__contacts">
+            <li className="contacts__landlinePhone contacts__item">
+              +7&nbsp;(343)&nbsp;382-19-67
+            </li>
+            <li className="contacts__mobilePhone contacts__item">
+              +7&nbsp;(902)&nbsp;444-41-92
+            </li>
+            <li className="contacts__email contacts__item">2060248@mail.ru</li>
+          </ul>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
