e4212653907f7e858765c7a45bba17a2f2cd634d
Valeriy Nikonovich <nikonovi4v@yandex.ru>
2023-10-16 09:17:42 +0300

    пофиксил карточки товара и полосу на футере

--- src/pages/PageCatalog/PageCatalog.jsx
+++ src/pages/PageCatalog/PageCatalog.jsx
@@ -1,9 +1,14 @@
 import "./PageCatalog.scss";
 import CatalogItem from "../../components/CatalogItem/CatalogItem";
 import { catalogList } from "../../utils/constants";
+import { Layout } from "../../Layout/Layout";
+import { NavigationAdress } from "../../components/NavigationAdress/NavigationAdress";
 export const PageCatalog = () => {
   return (
+    <>
+    <Layout theme="orange">
     <section className="page-catalog">
+    <NavigationAdress />
       <h2 className="page-catalog__title">Каталог</h2>
       <ul className="page-catalog__list">
         {catalogList.map((el, i) => (
@@ -11,5 +16,7 @@ export const PageCatalog = () => {
         ))}
       </ul>
     </section>
+    </Layout>
+    </>
   );
 };
